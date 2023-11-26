baca768939573c2ed4c079bf25477712f724532c
ekamotech <ekamo.tech@gmail.com>
2023-11-26 19:37:15 +0900

    Lesson 3の課題提出

---
+++ src/Review01.java
@@ -0,0 +1,30 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        // 商品価格
+        int price = 1500;
+
+        // 消費税額を計算
+        int tax = getTax(price);
+
+        // 税込価格を計算
+        int total = price + tax;
+
+        // 計算結果を表示
+        System.out.println(price + "円の商品の税込価格は" + total + "円（消費税は" + tax + "円）です。");
+    }
+
+    // 商品金額を受け取り、消費税額を計算するメソッド
+    public static int getTax(int price) {
+        // 税率
+        double rate = 0.10;
+
+        // 整数値になるようにキャスト（型変換）する
+        int tax = (int) (price * rate);
+
+        // 戻り値
+        return tax;
+    }
+
+}
